82e94aa68d94a6c874b88dd608c5840ca3a7c48e
Eugene Ivantsov <eivantsov@codenvy.com>
2016-07-13 16:10:44 +0300

    Update artik_pwm_test.c

--- artik_pwm_test.c
+++ artik_pwm_test.c
@@ -6,21 +6,24 @@
 #include <artik_platform.h>
 #include <artik_pwm.h>
 static artik_pwm_config config = {
-    ARTIK_A5_PWMIO_XPWMIO1,
+    ARTIK_A520_PWMIO_XPWMIO1,
     "pwm",
     400000,
     200000,
-    ARTIK_PWM_POLR_NORMAL
+    ARTIK_PWM_POLR_NORMAL,
+    NULL
 };
 static artik_error pwm_test_frequency(int platid)
 {
     artik_pwm_handle handle;
     artik_error ret = S_OK;
     artik_pwm_module *pwm = (artik_pwm_module *)artik_request_api_module("pwm");
-    if (platid == ARTIK5)
-        config.pin_num = ARTIK_A5_PWMIO_XPWMIO1;
+    if (platid == ARTIK520)
+        config.pin_num = ARTIK_A520_PWMIO_XPWMIO1;
+    else if (platid == ARTIK1020)
+        config.pin_num = ARTIK_A1020_PWMIO_XPWMIO1;
     else
-        config.pin_num = ARTIK_A10_PWMIO_XPWMIO1;
+        config.pin_num = ARTIK_A710_PWM0;
     fprintf(stdout, "TEST: %s\n", __func__);
     ret = pwm->request(&handle, &config);
     if (ret != S_OK) {
@@ -30,7 +33,7 @@ static artik_error pwm_test_frequency(int platid)
     /* Wait for X seconds */
     usleep(3 * 1000 * 1000);
     pwm->release(handle);
-    fprintf(stdout, "TEST: %s suceeded\n", __func__);
+    fprintf(stdout, "TEST: %s succeeded\n", __func__);
     artik_release_api_module(pwm);
     return ret;
 }
@@ -38,7 +41,7 @@ int main(void)
 {
     artik_error ret = S_OK;
     int platid = artik_get_platform();
-    if ((platid == ARTIK5) || (platid == ARTIK10))
+    if ((platid == ARTIK520) || (platid == ARTIK1020)  || (platid == ARTIK710))
         ret = pwm_test_frequency(platid);
     return (ret == S_OK) ? 0 : -1;
 }
